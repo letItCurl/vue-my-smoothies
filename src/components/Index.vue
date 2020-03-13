7ed8ac69ddf284111a9f6308ef546e5131211006
rollpz <rolandlopez.developer@gmail.com>
2020-03-13 05:18:21 +0100

    Data added and fetched from firestore

---
+++ src/components/Index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="(smoothie, index) in smoothies" :key="index">
+      <div class="card-content">
+          <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+          <h2 class="indigo-text">{{smoothie.title}}</h2>
+          <ul class="ingredients">
+            <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+              <span class="chip">{{ing}}</span>
+            </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      smoothies: []
+    }
+  },
+  methods: {
+    deleteSmoothie(id){
+      this.smoothies = this.smoothies.filter(smo => smo.id != id )
+    }
+  },
+  created(){
+    db.collection('smoothies').get()
+    .then(snapshot => snapshot.forEach(doc => {
+      let smoothie = doc.data()
+      smoothie.id = doc.id
+      this.smoothies.push(smoothie)
+    }))
+  }
+}
+</script>
+
+
+<style>
+.index{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.index .ingredients{
+  margin: 30px auto;
+}
+.index .ingredients li{
+  display: inline-block;
+}
+.index .delete{
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
+</style>
